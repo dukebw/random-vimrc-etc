8d622623ad3167711ed7930f3d7100db70c4872c
Brendan Duke <brendanw.duke@gmail.com>
2021-06-20 15:27:48 -0400

    Update script to pipe to bluetoothctl

--- connect-to-bose-qc-earbuds.sh
+++ connect-to-bose-qc-earbuds.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-bluetoothctl -- remove 60:AB:D2:EC:8D:CB
-sleep 5
-bluetoothctl -- pair 60:AB:D2:EC:8D:CB
-sleep 5
-bluetoothctl -- connect 60:AB:D2:EC:8D:CB
+{
+        printf 'remove 60:AB:D2:EC:8D:CB'
+        printf 'scan on'
+        sleep 5
+        printf 'connect 60:AB:D2:EC:8D:CB'
+} | bluetoothctl
