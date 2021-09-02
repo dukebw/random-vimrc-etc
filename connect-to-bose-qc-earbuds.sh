18a38cf9ed378e792e13b91f48c3a8936b7c7acf
Brendan Duke <brendanw.duke@gmail.com>
2021-09-02 10:47:26 -0400

    Update Bose QC earbud script

--- connect-to-bose-qc-earbuds.sh
+++ connect-to-bose-qc-earbuds.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-{
-        printf 'remove 60:AB:D2:EC:8D:CB'
-        printf 'scan on'
-        sleep 5
-        printf 'connect 60:AB:D2:EC:8D:CB'
-} | bluetoothctl
+bluetoothctl remove 60:AB:D2:EC:8D:CB
+bluetoothctl scan on
+bluetoothctl trust 60:AB:D2:EC:8D:CB
+bluetoothctl pair 60:AB:D2:EC:8D:CB
+bluetoothctl connect 60:AB:D2:EC:8D:CB
