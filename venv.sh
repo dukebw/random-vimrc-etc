b67e4650a1b270b5d303d31e25c72bfe37b9e566
Brendan Duke <brendanw.duke@gmail.com>
2024-10-28 11:44:23 +0000

    Add pytest-asyncio, debugpy, py-cpuinfo to venv

--- venv.sh
+++ venv.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+$MODULAR_PATH/bazelw run //SDK/public/max-repo/pipelines/python:pipelines.venv
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 .SDK+public+max-repo+pipelines+python+pipelines.venv/bin/python get-pip.py
-.SDK+public+max-repo+pipelines+python+pipelines.venv/bin/python -m pip install --upgrade pip python-lsp-server ruff python-lsp-ruff mypy pylsp-mypy pytest
+.SDK+public+max-repo+pipelines+python+pipelines.venv/bin/python -m pip install --upgrade pip python-lsp-server ruff python-lsp-ruff mypy pylsp-mypy pytest pytest-asyncio debugpy py-cpuinfo
 rm get-pip.py
