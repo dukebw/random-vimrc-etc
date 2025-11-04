336e4ef75e6c8b6f73ee230b507e4c6563d2572c
Brendan <bduke@modular.com>
2025-11-04 14:41:01 +0000

    typo

--- venv.sh
+++ venv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf .max+python+max+entrypoints+pipelines.venv/
-$MODULAR_PATH/bazelw run //maxpython/max/entrypoints:pipelines.venv
+$MODULAR_PATH/bazelw run //max/python/max/entrypoints:pipelines.venv
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 .max+python+max+entrypoints+pipelines.venv/bin/python get-pip.py
 .max+python+max+entrypoints+pipelines.venv/bin/python -m pip install --upgrade --ignore-installed pip python-lsp-server ruff python-lsp-ruff mypy pylsp-mypy pytest pytest-asyncio debugpy pre-commit datasets nvitop matplotlib
